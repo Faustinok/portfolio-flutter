3a14cf6237921269f7175b2305d60451ce4a5bd6
Gabriel <faustinoknada@gmail.com>
2020-06-17 22:52:26 -0300

    Continuação da criação do app de estoque

--- portfolio/countestoque/lib/screens/home.dart
+++ portfolio/countestoque/lib/screens/home.dart
@@ -1,17 +1,34 @@
-import 'package:countestoque/screens/components/cards.dart';
+import 'package:countestoque/screens/listaProdutos.dart';
+import 'package:countestoque/screens/conferirProdutos.dart';
 import 'package:flutter/material.dart';
+import 'cadastro.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _currentindex = 1;
+  final List<Widget> telas = [ConferirProdutos(), ListaProdutos(), Cadastro()];
+  trocaTela(int index) {
+    setState(() {
+      _currentindex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) => trocaTela(index),
         backgroundColor: Colors.lightBlue,
         items: [
           BottomNavigationBarItem(
             icon: Image.asset(
               "assets/caixa.png",
+              color: Colors.white,
               width: 35,
               height: 35,
             ),
@@ -20,26 +37,28 @@ class Home extends StatelessWidget {
               style: TextStyle(
                   fontSize: 16,
                   fontWeight: FontWeight.bold,
-                  color: Colors.black),
+                  color: Colors.white),
             ),
           ),
           BottomNavigationBarItem(
             icon: Image.asset(
               "assets/estoque.png",
+              color: Colors.white,
               width: 35,
               height: 35,
             ),
             title: Text(
-              "data",
+              "Produtos",
               style: TextStyle(
                   fontSize: 16,
                   fontWeight: FontWeight.bold,
-                  color: Colors.black),
+                  color: Colors.white),
             ),
           ),
           BottomNavigationBarItem(
             icon: Image.asset(
               "assets/mais.png",
+              color: Colors.white,
               width: 35,
               height: 35,
             ),
@@ -48,11 +67,12 @@ class Home extends StatelessWidget {
               style: TextStyle(
                   fontSize: 16,
                   fontWeight: FontWeight.bold,
-                  color: Colors.black),
+                  color: Colors.white),
             ),
           ),
         ],
       ),
+      body: telas[_currentindex],
     );
   }
 }
